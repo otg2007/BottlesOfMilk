fff7e8119f9298f14573c5000a9fff5477e1fa0b
otg2007 <161673694+otg2007@users.noreply.github.com>
2024-04-21 22:12:08 -0400

    Create .swift

---
+++ .swift
@@ -0,0 +1,8 @@
+// Write your code below 🥛
+var numBottles = 99
+while numBottles>0{
+  print("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!")
+  print("You take one down and pass it around...")
+  numBottles -= 1
+  print("\(numBottles) bottles of milk on the wall!")
+}
